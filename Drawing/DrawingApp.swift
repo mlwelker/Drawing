da21db178a8d86937069e7c99d0d238eea3ee48f
mlwelker <nutria.watts.0g@icloud.com>
2023-08-18 16:47:37 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Michael Welker on 2023-08-18.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
