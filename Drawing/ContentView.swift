36f153346a115bdba290227b3b042a8e04897491
mlwelker <nutria.watts.0g@icloud.com>
2023-08-18 22:47:59 -0500

    path triangle

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -1,21 +1,17 @@
-//
-//  ContentView.swift
-//  Drawing
-//
-//  Created by Michael Welker on 2023-08-18.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+//            path.closeSubpath()
         }
-        .padding()
+//        .fill(.blue)
+        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
 
