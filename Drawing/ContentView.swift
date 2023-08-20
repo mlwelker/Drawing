6680c3ca4df7a1ec9e41966e0e263f8f5612cbb6
mlwelker <nutria.watts.0g@icloud.com>
2023-08-19 19:08:35 -0500

    drawingGroup modifier Metal performance

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -1,44 +1,52 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
     
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+//                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5),
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
         
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
+        if targetHue > 1 {
+            targetHue -= 1
         }
         
-        return path
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-//                .stroke(.red, lineWidth: 1)
-                .fill(.red, style: FillStyle(eoFill: true))
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
             
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 1...100)
-                .padding(.horizontal)
+            Slider(value: $colorCycle)
         }
     }
 }
